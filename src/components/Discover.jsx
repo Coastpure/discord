3db5dd185541fb2158b1cb99411f54aaab0e2029
Coastpure <njorogesimon465@gmail.com>
2023-02-28 13:20:06 +0300

    Front-end done

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { AiFillCompass} from 'react-icons/ai';
 import { GiGamepad} from 'react-icons/gi';
 import { HiMusicNote} from 'react-icons/hi';
-import { FaGraduationCap} from 'react-icons/fa';
+import { FaGraduationCap, FaMicrophoneSlash} from 'react-icons/fa';
 import { VscSettingsGear} from 'react-icons/vsc';
 import { SlScreenDesktop} from 'react-icons/sl';
 import { BsFillEmojiSmileFill, BsHeadphones} from 'react-icons/bs';
 import Topic from './Topic';
+import lmfao from '../photos/lmfao.jpg'
 
 
 const Discover = () => {
@@ -23,20 +24,33 @@ const Discover = () => {
   ]
 
   return (
-    <div className='hidden xl:flex flex-col bg-[#303136] w-[18rem] shrink-0 p-4 h-screen sticky top-0 '>
-
-      {/* Title */}
-      <p className=' font-bold text-[24px] text-white'>Discover</p>
-      {/* Topics */}
-      <div className="mt-4 space-y-4">
-        {topics.map((topic) => (
-          <Topic title = {topic.title} icon = {topic.icon}/>
-        ))}
+    <div className='hidden xl:flex flex-col justify-between bg-[#303136] w-[13rem] shrink-0  h-screen sticky top-0 '>
+      {/* Title + Topics */}
+      <div className="p-4">
+          {/* Title */}
+          <p className=' font-bold text-[24px] text-white'>Discover</p>
+          {/* Topics */}
+          <div className="mt-4 space-y-4">
+          {topics.map((topic) => (
+              <Topic title = {topic.title} icon = {topic.icon}/>
+            ))}
+          </div>
       </div>
       {/* ProfileSettings */}
-      <div className="bg-[#292b2f]">
-
-      </div>
+        <div className="bg-[#292b2f] h-[3rem] flex items-center text-white px-2 justify-between">
+          <div className="flex items-center">
+            <img src={lmfao} alt="" className='w-10 h-10 rounded-full'/>
+            <div className="pl-2">
+              <p className='text-[12px] font-semibold'>coastpure</p>
+              <p className='text-[10px] text-gray-400'>#1234</p>
+            </div>
+            </div>
+            <div className="flex space-x-3 text-[15px] text-white/80">
+              <FaMicrophoneSlash />
+              <BsHeadphones />
+              < VscSettingsGear />
+            </div>
+        </div>
     </div>
   )
 }
--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -10,31 +10,31 @@ import { AiFillCompass} from 'react-icons/ai';
 const Groups = () => {
   const groups = [img1, img2, img3, img4, img1, img2]
   return (
-    <div className='hidden sm:flex bg-[#212226] w-[6rem] shrink-0 h-screen sticky top-0 pt-6'>
+    <div className='hidden sm:flex bg-[#212226] w-[4rem] shrink-0 h-screen sticky top-0 pt-6'>
         
         
         {/* Groups */}
         <div className="flex flex-col items-center w-full space-y-4">
         {/* Discord Avi*/}
-        <div className='bg-[#36393f] p-1 rounded-[1.3rem] hover:rounded-full duration-100 ease-in-out'> 
-            <img src={dc} className="w-12 h-12 brightness-[80%]"/>
+        <div className='bg-[#36393f] p-0.5 rounded-[1.3rem] hover:rounded-full duration-100 ease-in-out cursor-pointer'> 
+            <img src={dc} className="w-10 h-10 brightness-[80%]"/>
         </div>
 
         {/* Avatars */}
         <div className="space-y-2 pt-3">
           {groups.map((group) => ( 
-        <div className='w-14 h-14 flex'> 
+        <div className='w-12 h-12 flex cursor-pointer' key={group}> 
             <img src={group} className="object-cover rounded-full hover:rounded-[1.3rem] duration-100 ease-in-out"/>
         </div>
          ))}
         </div>
 
          {/* plus */}
-         <div className='bg-[#36393f] p-4 rounded-full hover:rounded-[1.3rem] duration-100 ease-in-out'> 
-            <FiPlus className="w-6 h-6 text-green-700"/>
+         <div className='bg-[#36393f] p-3 rounded-full hover:rounded-[1.3rem] duration-100 ease-in-out'> 
+            <FiPlus className="w-6 h-6 text-green-700 cursor-pointer"/>
         </div>
         {/* compass */}
-        <div className='bg-[#3ba55d] p-3 rounded-[1.3rem] hover:rounded-full duration-100 ease-in-out'> 
+        <div className='bg-[#3ba55d] p-2 rounded-[1.3rem] hover:rounded-full duration-100 ease-in-out cursor-pointer'> 
             <AiFillCompass className="w-8 h-8 text-white"/>
         </div>
       </div>
